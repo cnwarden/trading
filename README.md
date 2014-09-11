1ebccca5949a484d99e6bd37d3490583c9c0611b
cnwarden <cnwarden@gmail.com>
2014-09-11 08:27:22 +0800

    extra ideas

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-TradingPlatform
+Trading Simulator
 ===============
 
-Trading Platform simulator
+[![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-pylint.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-pylint)
+
+This linter plugin for [SublimeLinter][docs] provides an interface to [pylint](http://www.pylint.org/). It will be used with files that have the “python” syntax.
+
+## Installation
+SublimeLinter 3 must be installed in order to use this plugin. If SublimeLinter 3 is not installed, please follow the instructions [here][installation].
+
+### Linter installation
+Before using this plugin, you must ensure that `pylint` is installed on your system. To install `pylint`, do the following:
+
+
+## Contributing
+If you would like to contribute enhancements or fixes, please do the following:
+
+1. Fork the plugin repository.
+1. Hack on a separate topic branch created from the latest `master`.
+1. Commit and push the topic branch.
+1. Make a pull request.
+1. Be patient.  ;-)
+
+[docs]: http://sublimelinter.readthedocs.org
--- unit_test.py
+++ unit_test.py
@@ -1,24 +1,19 @@
 
-
-from trading.server.udpserver import *
-from trading.engine.order import *
-from trading.engine.market import *
-from trading.engine.instrument import *
-
+from trading.engine.order import Order
+from trading.engine.market import Market
+from trading.engine.instrument import Instrument
 import unittest
 
-class TestInstrument(unittest.TestCase):
+"""
+unittest suite of trading module
+"""
 
+class TestInstrument(unittest.TestCase):
+    """
+    test instrument functions
+    """
     def setUp(self):
         pass
-
-    def test_startup(self):
-        server = UdpServer()
-        server.open()
-        for i in xrange(10):
-            server.send('testing')
-        server.close()
-
 
     def test_order(self):
         instrument = Instrument(id='APPL')
@@ -36,7 +31,6 @@ class TestInstrument(unittest.TestCase):
         instrument.insert(Order(id=4, price=4.0, quantity=100, side=OrderSide.BUY))
         instrument.insert(Order(id=5, price=5.0, quantity=100, side=OrderSide.BUY))
 
-
         instrument.insert(Order(id=6, price=5.0, quantity=100, side=OrderSide.SELL))
 
         instrument.match()
@@ -50,15 +44,10 @@ class TestInstrument(unittest.TestCase):
         instrument.insert(Order(id=5, price=5.0, quantity=200, side=OrderSide.BUY))
         instrument.insert(Order(id=7, price=5.0, quantity=200, side=OrderSide.BUY))
 
-
         instrument.insert(Order(id=6, price=5.0, quantity=200, side=OrderSide.SELL))
 
-        print instrument
-        
         instrument.match()
 
-        print instrument
-        
     def test_aggregate_match_bid_less_ask(self):
         instrument = Instrument(id='APPL')
         instrument.insert(Order(id=4, price=4.0, quantity=100, side=OrderSide.BUY))
@@ -68,14 +57,22 @@ class TestInstrument(unittest.TestCase):
         instrument.insert(Order(id=5, price=5.0, quantity=200, side=OrderSide.BUY))
         instrument.insert(Order(id=7, price=5.0, quantity=200, side=OrderSide.BUY))
 
-
         instrument.insert(Order(id=6, price=5.0, quantity=600, side=OrderSide.SELL))
 
-        print instrument
-        
         instrument.match()
 
-        print instrument
+    def test_aggregate_match_bid_equal_ask(self):
+        instrument = Instrument(id='APPL')
+        instrument.insert(Order(id=4, price=4.0, quantity=100, side=OrderSide.BUY))
+        instrument.insert(Order(id=3, price=2.0, quantity=100, side=OrderSide.BUY))
+        instrument.insert(Order(id=1, price=3.0, quantity=100, side=OrderSide.BUY))
+        instrument.insert(Order(id=2, price=5.0, quantity=100, side=OrderSide.BUY))
+        instrument.insert(Order(id=5, price=5.0, quantity=200, side=OrderSide.BUY))
+        instrument.insert(Order(id=7, price=5.0, quantity=200, side=OrderSide.BUY))
+
+        instrument.insert(Order(id=6, price=5.0, quantity=500, side=OrderSide.SELL))
+
+        instrument.match()
 
     def tearDown(self):
         pass
@@ -83,6 +80,10 @@ class TestInstrument(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    cases = ['test_order']
+    cases = ['test_order',
+             'test_match',
+             'test_aggregate_match_bid_bigger_ask',
+             'test_aggregate_match_bid_less_ask',
+             'test_aggregate_match_bid_equal_ask']
     suite = unittest.TestSuite(map(TestInstrument, cases))
     unittest.TextTestRunner(verbosity=2).run(suite)
